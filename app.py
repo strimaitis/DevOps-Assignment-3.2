77a3854c1260d24a05501184139c7aadc60c80a1
Matthew Strimaitis <matthew.strimaitis@gmail.com>
2025-07-11 20:32:56 -0500

    Fixing most lint errors

--- app.py
+++ app.py
@@ -1,26 +1,49 @@
-from flask import Flask, jsonify
+'''
+  App.py: Application that displays home web page, accesses pokemon data from API endpoints 
+  and adds numbers
+'''
+
 from urllib.request import urlopen
 import json
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    """
+    Displays instructions on how to access /pokedex
+    Args: N/A
+    """
     return jsonify(message="Welcome, enter /pokedex into the base URL to see list of Pokemon")
 
 @app.route('/pokedex')
 def pokedex():
+    """
+    Displays pokemon from endpoint
+    Args: N/A
+    """
     url = "https://pokeapi.co/api/v2/pokemon/"
     response = urlopen(url)
     data_json = json.loads(response.read())
-    return jsonify(message="{}".format(data_json))
+    return jsonify(message=f"{data_json}")
 
 @app.route('/about')
 def about():
+    """
+    Displays static json
+    Args: N/A
+    """
     return jsonify(message="This is the about page")
 
 @app.route('/add/<int:x>/<int:y>')
 def multiply(x, y):
+    """
+    Returns the sum of two integers
+    Args:
+    x: Integer
+    y: Integer
+    """
     result = x + y
     return jsonify(result=result)
 
